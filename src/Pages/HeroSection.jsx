7a968ef7b0b1dab708657edf52cdcdcb44c52c94
Saran-pariyar <spariyar05@gmail.com>
2022-08-26 16:13:52 +0545

    Made category section and going to add card

--- src/Pages/HeroSection.jsx
+++ src/Pages/HeroSection.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "../Assets/css/HeroSection.css";
+import mac from "../Assets/img/mac.jpg";
+import mobile from "../Assets/img/mobile.jpg";
+import sunglasses from "../Assets/img/sunglass.jpg";
 
 const HeroSection = () => {
+  const categories = [
+    { smartphones: mobile },
+    { laptops: mac },
+    { sunglasses: sunglasses },
+  ];
+  // smartphones', 'laptops', 'fragrances', 'sunglasses', 'automotive', 'motorcycle',
   return (
     <section className="hero-section">
       {/* <a href="https://www.etelligens.com/" style>Click to see demo</a> */}
@@ -13,9 +22,16 @@ const HeroSection = () => {
           The shop for the <br />
           <span> #Genz</span>
         </h1>
-        <a href="#item" className="explore-btn">
+        <a href="#categories" className="explore-btn">
           Explore!
         </a>
+      </div>
+      {/* categories section  */}
+      <div className="categories-section" id="categories">
+        <h2 className="categories-heading">Categories</h2>
+
+        {/* //card section  */}
+        <div className="categories-card-section"></div>
       </div>
     </section>
   );
